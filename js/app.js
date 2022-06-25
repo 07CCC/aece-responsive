4829fc67f3542aae33410e5793163f59c341cd61
07CCC <104282617+07CCC@users.noreply.github.com>
2022-06-25 16:44:23 -0500

    Responsive app.js

--- js/app.js
+++ js/app.js
@@ -5,12 +5,42 @@ $(".aliados-slideshow").slick({
     autoplay: true,
     autoplaySpeed: 2500,
     arrows: false,
+    responsive: [
+        {
+          breakpoint: 977,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+            breakpoint: 690,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 456,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+        }
+    ]
 });
 
 $(".cursos-slideshow").slick({
     slidesToScroll: 1,
-    prevArrow: '.cursos-slideshow__arrow-left',
-    nextArrow: '.cursos-slideshow__arrow-right',
+    responsive: [
+        {
+          breakpoint: 1220,
+          settings: {
+            arrows: false,
+            dots: true
+          }
+        }
+    ]
 });
 
 sal();
